bc47ade830a58d95ed9e72a7a2f172142895d8e9
rafi-windows <rafi_n_o@github.com>
2020-09-12 13:24:14 +0700

    menambahkan fitur UPLOAD GAMBAR Part 1

--- kuliah/pertemuan13/functions.php
+++ kuliah/pertemuan13/functions.php
@@ -24,6 +24,53 @@ function query($query)
     return $data;
 }
 
+function upload()
+{
+    $nama_file = $_FILES['gambar']['name'];
+    $tipe_file = $_FILES['gambar']['type'];
+    $ukuran_file = $_FILES['gambar']['size'];
+    $error = $_FILES['gambar']['error'];
+    $tmp_file = $_FILES['gambar']['tmp_name'];
+
+    // ketika tidak ada gambar yang dipilih
+    if ($error == 4) {
+        echo "<script>alert('pilih gambar terlebih dahulu!')</script>";
+        return false;
+    }
+
+    // cek ekstensi file
+    $daftar_gambar = ['jpg', 'jpeg', 'png'];
+    $ekstensi_file = explode('.', $nama_file);
+    $ekstensi_file = strtolower(end($ekstensi_file));
+    if (!in_array($ekstensi_file, $daftar_gambar)) {
+        echo "<script>alert('format gambar tidak didukung!')</script>";
+        return false;
+    }
+
+    // cek type file
+    if ($tipe_file != 'image/jpeg' && $tipe_file != 'image/png') {
+        echo "<script>alert('format gambar tidak didukung!')</script>";
+        return false;
+    }
+
+    // cek ukuran file
+    // maksimal 2Mb == 2000000
+    if ($ukuran_file > 2000000) {
+        echo "<script>alert('ukuran terlalu besar!')</script>";
+        return false;
+    }
+
+    // lolos pengecekan
+    // siap upload file
+    // generate nama file baru
+    $nama_file_baru = uniqid();
+    $nama_file_baru .= '.';
+    $nama_file_baru .= $ekstensi_file;
+    move_uploaded_file($tmp_file, 'img/' . $nama_file_baru);
+
+    return $nama_file_baru;
+}
+
 function tambah($data)
 {
     $conn = koneksi();
@@ -32,7 +79,13 @@ function tambah($data)
     $nim = htmlspecialchars($data['nim']);
     $email = htmlspecialchars($data['email']);
     $jurusan = htmlspecialchars($data['jurusan']);
-    $gambar = htmlspecialchars($data['gambar']);
+    // $gambar = htmlspecialchars($data['gambar']);
+
+    // upload gambar
+    $gambar = upload();
+    if (!$gambar) {
+        return false;
+    }
 
     $query = "INSERT INTO mahasiswa VALUES (null, '$nama', '$nim', '$email', '$jurusan', '$gambar')";
     mysqli_query($conn, $query) or die(mysqli_error($conn));
